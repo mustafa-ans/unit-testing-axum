dd808b8dbe51cf33bc49f5ca2e3053640e020d3c
Mustafa-ans <112391427+Mustafa-ans@users.noreply.github.com>
2023-11-06 17:21:41 +0530

    Fix database error handling and add unit tests for delete and update user routes

--- src/controllers/delete_user.rs
+++ src/controllers/delete_user.rs
@@ -11,15 +11,16 @@ pub async fn delete_user(
     Query(update_user): Query<DeleteUser>,
 ) -> Result<Json<Value>, String> {
     // Check if the user with the given username exists
-    let user_exists = sqlx::query_scalar::<_, i32>(
+    let user_exists = sqlx::query_scalar::<_, i64>(
         "SELECT COUNT(*) FROM Users WHERE Username = $1",
     )
     .bind(&update_user.username)
     .fetch_one(&pool)
     .await
     .map_err(|err| {
-        dbg!(err);
-        "Error checking user existence".to_string()
+        let err_message = format!("Error checking if user exists, error returned from the database: \n{}", err);
+        println!("{}",&err_message);
+        err_message
     })?;
 
     if user_exists == 0 {
@@ -51,4 +52,91 @@ pub async fn delete_user(
     })?;
 
     Ok(Json(json!({"message": format!("User '{}' deleted successfully.", deleted_user)})))
+}
+
+
+#[cfg(test)]
+mod tests {
+    use axum::{http::{Request, Method}, body::Body, routing::{get, post, delete} , Router};
+    use sqlx_core::postgres::PgPoolOptions;
+    use tower::util::ServiceExt;
+    use super::*;
+    
+    #[tokio::test]
+    async fn check_database_connectivity(){
+        let durl = std::env::var("DATABASE_URL_ONLIN").expect("set DATABASE_URL env variable");
+
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&durl)
+            .await
+            .expect("unable to make connection");
+
+        // assert to check if connection is successful
+        assert_eq!(pool.is_closed(),false);
+
+    }
+
+    async fn create_connection_pool() -> PgPool{
+        let durl = std::env::var("DATABASE_URL_ONLINE").expect("set DATABASE_URL env variable");
+
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&durl)
+            .await
+            .expect("unable to make connection");
+
+        return pool;
+    }
+
+    #[tokio::test]
+    async fn test_delete_user_route(){
+        let pool = create_connection_pool().await;
+        let app = Router::new()
+            .route("/delete_user", delete(delete_user))
+            .layer(Extension(pool));
+
+        // send a request to the route with query parameters
+
+        let req = Request::builder()
+            .method(Method::DELETE)
+            .uri("/delete_user?username=johndoe87")
+            .body(Body::empty())
+            .unwrap();
+
+        let res = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        // check the response body is as expected
+        let body = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        assert_eq!(body, r#"{"message":"User 'johndoe87' deleted successfully."}"#);
+    }
+
+    #[tokio::test]
+    async fn test_delete_user_route_user_not_found(){
+        let pool = create_connection_pool().await;
+        let app = Router::new()
+            .route("/delete_user", delete(delete_user))
+            .layer(Extension(pool));
+
+        // send a request to the route with query parameters
+
+        let req = Request::builder()
+            .method(Method::DELETE)
+            .uri("/delete_user?username=TestUser")
+            .body(Body::empty())
+            .unwrap();
+
+        let res = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        // check the response body is as expected
+        let body = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        assert_eq!(body, r#"User not found. Cannot delete user."#);
+    }
+
 }
--- src/controllers/edit_user.rs
+++ src/controllers/edit_user.rs
@@ -12,15 +12,16 @@ pub async fn update_user(
     Json(update_user): Json<UpdateUser>,
 ) -> Result<Json<Value>, String> {
     // Check if the user with the given username exists
-    let user_exists = sqlx::query_scalar::<_, i32>(
+    let user_exists = sqlx::query_scalar::<_, i64>(
         "SELECT COUNT(*) FROM Users WHERE Username = $1",
     )
     .bind(&update_user.username)
     .fetch_one(&pool)
     .await
     .map_err(|err| {
-        dbg!(err);
-        "Error checking user existence".to_string()
+        let err_message = format!("Error checking if user exists, error returned from the database: \n{}", err);
+        println!("{}",&err_message);
+        err_message
     })?;
 
     if user_exists == 0 {
@@ -32,11 +33,11 @@ pub async fn update_user(
         r#"
         UPDATE Users
         SET
-            First_Name = COALESCE($2, First_Name),
-            Last_Name = COALESCE($3, Last_Name),
-            Email = COALESCE($4, Email),
-            BirthDate = COALESCE($5, BirthDate)
-        WHERE Username = $1
+            firstName = COALESCE($2, firstName),
+            lastName = COALESCE($3, lastName),
+            email = COALESCE($4, email),
+            birthdate = COALESCE($5, birthdate)
+        WHERE username = $1
         "#,
     )
     .bind(&update_user.username)
@@ -52,4 +53,109 @@ pub async fn update_user(
     })?;
 
     Ok(Json(json!({"message": "User details updated successfully."})))
+}
+
+
+#[cfg(test)]
+mod tests {
+    use axum::{http::{Request, Method}, body::Body, routing::put , Router};
+    use sqlx_core::postgres::PgPoolOptions;
+    use tower::util::ServiceExt;
+    use super::*;
+
+    #[tokio::test]
+    async fn check_database_connectivity(){
+        let durl = std::env::var("DATABASE_URL_ONLIN").expect("set DATABASE_URL env variable");
+
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&durl)
+            .await
+            .expect("unable to make connection");
+
+        // assert to check if connection is successful
+        assert_eq!(pool.is_closed(),false);
+
+    }
+
+    async fn create_connection_pool() -> PgPool{
+        let durl = std::env::var("DATABASE_URL_ONLINE").expect("set DATABASE_URL env variable");
+
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(&durl)
+            .await
+            .expect("unable to make connection");
+
+        return pool;
+    }
+    #[tokio::test]
+    async fn test_update_user_route(){
+        let pool = create_connection_pool().await;
+        let app = Router::new()
+            .route("/update-users",put(update_user))
+            .layer(Extension(pool));
+
+        let req = Request::builder()
+            .method(Method::PUT)
+            .uri("/update-users")
+            .header("content-type", "application/json")
+            .body(Body::from(
+                r#"{
+                    "username": "Test",
+                    "first_name": "Test",
+                    "last_name": "User",
+                    "email": "test@mail",
+                    "birthdate": "2020-01-01"
+                }"#,
+            ))
+            .unwrap();
+
+        let resp = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        // extract the response body as bytes
+        let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
+
+        // assert to check if user details are updated successfully 
+        assert_eq!(body, r#"{"message":"User details updated successfully."}"#);
+                
+    }
+
+    #[tokio::test]
+    async fn test_update_user_not_found(){
+        let pool = create_connection_pool().await;
+        let app = Router::new()
+            .route("/update-users",put(update_user))
+            .layer(Extension(pool));
+
+        let req = Request::builder()
+            .method(Method::PUT)
+            .uri("/update-users")
+            .header("content-type", "application/json")
+            .body(Body::from(
+                r#"{
+                    "username": "Test",
+                    "first_name": "Test",
+                    "last_name": "User",
+                    "email": "test@mail",
+                    "birthdate": "2020-01-01"
+                }"#,
+            ))
+            .unwrap();
+
+        let resp = app
+            .oneshot(req)
+            .await
+            .unwrap();
+
+        // extract the response body as bytes
+        let body = hyper::body::to_bytes(resp.into_body()).await.unwrap();
+
+        // assert to check if user was not found
+        assert_eq!(body, r#"User not found. Cannot update user details."#);
+    }
+
 }
